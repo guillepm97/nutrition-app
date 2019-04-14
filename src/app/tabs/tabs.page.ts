d0a40f053660f9e17420ef04fc3b1d97cc082b22
RaulJC <37372071+RaulJC@users.noreply.github.com>
2019-04-14 02:55:41 +0200

    Differentiate by type of user (#6)

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage {
+  currentUserNutritionist: boolean;
+
+  constructor(private router: Router, private route: ActivatedRoute,
+              private userService: UserService) {}
+
+  ngOnInit() {
+    this.currentUserNutritionist = this.userService.currentUserNutritionist();
+    this.redirectToCorrectTab();
+  }
+
+  redirectToCorrectTab() {
+    if (this.currentUserNutritionist) {
+      this.router.navigate(['./clients'], { relativeTo: this.route });
+    } else {
+      this.router.navigate(['./diary'], { relativeTo: this.route });
+    }
+  }
+}
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@angular/core';
 
-import { User } from './user';
+import { User, UserType } from './user';
 import { USERS } from './mock-users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  currentUserId: number = 1;
 
   constructor() { }
 
   getUsers(): User[] {
     return USERS;
+  }
+
+  getUser(id: number): User {
+    for (let user of this.getUsers()) {
+      if (user.id == id) {
+        return user;
+      }
+    }
+
+    return null;
+  }
+
+  getCurrentUser(): User {
+    return this.getUser(this.currentUserId);
+  }
+
+  currentUserNutritionist(): boolean {
+    return this.getCurrentUser().type == UserType.Nutritionist;
   }
 }
